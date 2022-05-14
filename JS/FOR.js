d7c7e6accaa5b781626d53e9d73120b4476bda14
Giovanni <giovannibsf@gmail.com>
2022-05-14 17:17:16 -0300

    anterações

--- JS/FOR.js
+++
@@ -1,9 +0,0 @@
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
-for (let index = 0; index < groceryList.length; index++) {
-    console.log(groceryList[index])
-}
-
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
-for(let nomes of names) {
-    console.log(nomes)
-}
--- JS/arrays.js
+++
@@ -1,3 +0,0 @@
-let menu =['home' , 'serviços', 'portfolio' , 'links']
-menu.push("contato");
-console.log(menu)
