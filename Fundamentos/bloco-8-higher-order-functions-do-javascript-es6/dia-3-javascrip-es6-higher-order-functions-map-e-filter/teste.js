da3e4a2adea84cc101d2c1d927c284dab126e0a0
Giovanni <giovannibsf@gmail.com>
2022-05-22 18:51:59 -0300

    exercícios do dia 4, bloco 8

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascrip-es6-higher-order-functions-map-e-filter/teste.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascrip-es6-higher-order-functions-map-e-filter/teste.js
@@ -7,4 +7,69 @@ const removeStudentByName = (name, listStudents) =>
   // Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
 
 const newListStudents = removeStudentByName('Jorge', arrayMyStudents);
-console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
+
+
+
+
+
+animais = [
+  {
+    nome: 'Fumaça',
+    idade: 3,
+    tipo: 'cao'
+  },
+  {
+    nome: 'Tobby',
+    idade: 6,
+    tipo: 'cao'
+  },
+  {
+    nome: 'Laminha',
+    idade: 1,
+    tipo: 'gato'
+  },
+  {
+    nome: 'Nutella',
+    idade: 3,
+    tipo: 'cao'
+  },
+  ];
+
+  
+ const reduz = () => { 
+  return animais.reduce((acc, crr) =>{
+  if (crr.tipo === 'cao') {
+    return acc += (crr.idade * 7);
+  } else return acc
+}, 0)
+ }
+
+ console.log(reduz())
+
+
+
+ //   let nomes = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice', 'Bruce'];
+
+// let quantidadeNomes = nomes.reduce( (todosNomes, nome) => {
+//   console.log(todosNomes)
+//   console.log(nome)
+//   if(nome in todosNomes) {
+//     console.log(todosNomes )
+//     todosNomes[nome]++
+//     console.log(todosNomes)
+//   } else{
+//     todosNomes[nome] = 1
+//   }
+//   return todosNomes
+// }, {});
+
+
+var numeros = [0, 1, 2, 3, 4, 5, 6];
+var numeros1 = [15, 20, 20, 45, 67]
+
+var total = numeros.reduce((acc, crr) => acc + crr, 
+numeros1.reduce((acc, crr) => acc+crr))
+
+
+
+
