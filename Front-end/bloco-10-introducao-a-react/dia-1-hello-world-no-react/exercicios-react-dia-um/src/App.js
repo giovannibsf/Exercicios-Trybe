1535a8bf5048b28de6ade63ca1e3ecbcb5213cac
Giovanni <giovannibsf@gmail.com>
2022-06-22 15:03:59 -0300

    exercicios bloco 10

---
+++ Front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-react-dia-um/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Tarefas from './task';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Tarefas/>
+      
+    );
+  }
+}
+
+export default App;
